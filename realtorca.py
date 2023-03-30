3aa8f981b859f87df7c4d9957b9d9579333efa65
Harry Grewal <46252218+harry-s-grewal@users.noreply.github.com>
2023-03-30 16:30:55 -0400

    Added error checking, Improved rate limiting avoidance

--- realtorca.py
+++ realtorca.py
@@ -14,7 +14,7 @@ def getCoordinates(city):
             return response["boundingbox"] # [latMin, latMax, lonMin, lonMax]
     return data
 
-def sendQuery(latMin, latMax, longMin, longMax, priceMin=0, priceMax=10000000, recordsPerPage=200, cultureId=1, currentPage=1, applicationId=1):
+def sendPropertyListQuery(latMin, latMax, longMin, longMax, priceMin=0, priceMax=10000000, recordsPerPage=200, cultureId=1, currentPage=1, applicationId=1):
     url = "https://api2.realtor.ca/Listing.svc/PropertySearch_Post"
     headers = {"Referer": "https://www.realtor.ca/",
                 "Origin": "https://www.realtor.ca/",
@@ -32,24 +32,34 @@ def sendQuery(latMin, latMax, longMin, longMax, priceMin=0, priceMax=10000000, r
         "ApplicationId": applicationId
     }
     response = requests.post(url=url, headers=headers, data=form)
+    if response.status_code == 403:
+        print("Error 403: Rate limited")
+    elif response.status_code != 200:
+        print("Error " + str(response.status_code))
     response.raise_for_status()
     return response.json()
 
-def getProperties(city):
-    coords = getCoordinates(city)
+def getPropertyListByCity(city):
+    coords = getCoordinates(city) # Creates bounding box for city
     maxPages = 1
     currentPage = 1
     results = []
     while currentPage <= maxPages:
-        data = sendQuery(coords[0], coords[1], coords[2], coords[3], currentPage=currentPage)
-        maxPages = math.ceil(data["Paging"]["TotalRecords"]/data["Paging"]["RecordsPerPage"])
-        results.append(data["Results"])
-        currentPage += 1
-        sleep(randint(600,900)) # sleep for 10-15 minutes to avoid getting blocked
+        try:
+            data = sendPropertyListQuery(coords[0], coords[1], coords[2], coords[3], currentPage=currentPage)
+            maxPages = math.ceil(data["Paging"]["TotalRecords"]/data["Paging"]["RecordsPerPage"])
+            results.append(data["Results"])
+            currentPage += 1
+            sleep(randint(600,900)) # sleep for 10-15 minutes to avoid rate-limiting
+        except:
+            print("Error: " + city)
+            sleep(randint(3000,3600)) # sleep for 50-60 minutes if frozen out
     return results
 
-df = pd.DataFrame()
-propertiesList = getProperties("Calgary, AB")
-for json in propertiesList:
-    df = df.append(pd.json_normalize(json))
-df.to_csv("properties.csv")
+def saveResultsToCSV(city):
+    df = pd.DataFrame()
+    propertiesList = getPropertyListByCity(city)
+    for json in propertiesList:
+        df = df.append(pd.json_normalize(json))
+    filename = city.replace(" ","").replace(",","") + ".csv"
+    df.to_csv(filename)
