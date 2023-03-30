423ab8ecec302e98a366333b18189257fd787aae
Harry Grewal <46252218+harry-s-grewal@users.noreply.github.com>
2023-03-29 23:43:56 -0400

    Loop through number of pages and save to csv

--- realtorca.py
+++ realtorca.py
@@ -1,4 +1,10 @@
 import requests, json
+import math
+
+url = "https://api2.realtor.ca/Listing.svc/PropertySearch_Post"
+headers = {"Referer": "https://www.realtor.ca/",
+               "Origin": "https://www.realtor.ca/",
+               "Host": "api2.realtor.ca"}
 
 def getCoordinates(city):
     url = "https://nominatim.openstreetmap.org/search?q=" + city + "&format=json&country=Canada"
@@ -25,15 +31,24 @@ def makeForm(latMin, latMax, longMin, longMax, priceMin=0, priceMax=10000000, re
     }
     return form
 
-
-
 def getProperties(city):
     coords = getCoordinates(city)
-    form = makeForm(coords[0], coords[1], coords[2], coords[3])
-    url = "https://api2.realtor.ca/Listing.svc/PropertySearch_Post"
-    headers = {"Referer": "https://www.realtor.ca/",
-               "Origin": "https://www.realtor.ca/",
-               "Host": "api2.realtor.ca"}
-    response = requests.post(url=url, headers=headers, data=form)
-    response.raise_for_status()
-    return response.json()
+    maxPages = 1
+    currentPage = 1
+    results = []
+    while currentPage <= maxPages:
+        form = makeForm(coords[0], coords[1], coords[2], coords[3], currentPage=currentPage)
+        response = requests.post(url=url, headers=headers, data=form)
+        response.raise_for_status()
+        data = response.json()
+        maxPages = math.ceil(data["Paging"]["TotalRecords"]/data["Paging"]["RecordsPerPage"])
+        results.append(data["Results"])
+        currentPage += 1
+    return results
+
+import pandas as pd
+df = pd.DataFrame()
+propertiesList = getProperties("Victoria, BC")
+for json in propertiesList:
+    df = df.append(pd.json_normalize(json))
+df.to_csv("properties.csv")
