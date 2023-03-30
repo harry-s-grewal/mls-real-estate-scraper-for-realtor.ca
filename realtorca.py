e2d5f4047acc504ad38ca8ee35ae1f4ce86b278d
Harry Grewal <46252218+harry-s-grewal@users.noreply.github.com>
2023-03-30 13:15:43 -0400

    Query list of properties with sleep to avoid rate-limit

--- realtorca.py
+++ realtorca.py
@@ -1,10 +1,8 @@
 import requests, json
+import pandas as pd
 import math
-
-url = "https://api2.realtor.ca/Listing.svc/PropertySearch_Post"
-headers = {"Referer": "https://www.realtor.ca/",
-               "Origin": "https://www.realtor.ca/",
-               "Host": "api2.realtor.ca"}
+from time import sleep
+from random import randint
 
 def getCoordinates(city):
     url = "https://nominatim.openstreetmap.org/search?q=" + city + "&format=json&country=Canada"
@@ -16,7 +14,11 @@ def getCoordinates(city):
             return response["boundingbox"] # [latMin, latMax, lonMin, lonMax]
     return data
 
-def makeForm(latMin, latMax, longMin, longMax, priceMin=0, priceMax=10000000, recordsPerPage=200, cultureId=1, currentPage=1, applicationId=1):
+def sendQuery(latMin, latMax, longMin, longMax, priceMin=0, priceMax=10000000, recordsPerPage=200, cultureId=1, currentPage=1, applicationId=1):
+    url = "https://api2.realtor.ca/Listing.svc/PropertySearch_Post"
+    headers = {"Referer": "https://www.realtor.ca/",
+                "Origin": "https://www.realtor.ca/",
+                "Host": "api2.realtor.ca"}
     form = {
         "LatitudeMin": latMin,
         "LatitudeMax": latMax,
@@ -29,7 +31,9 @@ def makeForm(latMin, latMax, longMin, longMax, priceMin=0, priceMax=10000000, re
         "CurrentPage": currentPage,
         "ApplicationId": applicationId
     }
-    return form
+    response = requests.post(url=url, headers=headers, data=form)
+    response.raise_for_status()
+    return response.json()
 
 def getProperties(city):
     coords = getCoordinates(city)
@@ -37,18 +41,15 @@ def getProperties(city):
     currentPage = 1
     results = []
     while currentPage <= maxPages:
-        form = makeForm(coords[0], coords[1], coords[2], coords[3], currentPage=currentPage)
-        response = requests.post(url=url, headers=headers, data=form)
-        response.raise_for_status()
-        data = response.json()
+        data = sendQuery(coords[0], coords[1], coords[2], coords[3], currentPage=currentPage)
         maxPages = math.ceil(data["Paging"]["TotalRecords"]/data["Paging"]["RecordsPerPage"])
         results.append(data["Results"])
         currentPage += 1
+        sleep(randint(600,900)) # sleep for 10-15 minutes to avoid getting blocked
     return results
 
-import pandas as pd
 df = pd.DataFrame()
-propertiesList = getProperties("Victoria, BC")
+propertiesList = getProperties("Calgary, AB")
 for json in propertiesList:
     df = df.append(pd.json_normalize(json))
 df.to_csv("properties.csv")
